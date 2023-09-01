4185a1f38411e65737a339a4b9196021a891bfbf
Mikhail Abramishvili <mg@abramishvili.net>
2023-09-01 12:06:39 +0400

    .

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -17,6 +17,13 @@
     <link rel="stylesheet" href="/css/flickity-fade.css">
     @vite('resources/css/front.css')
 
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="theme-color" content="#ffffff">
+
     @if($settings->header)
         {!! $settings->header !!}
     @endif
