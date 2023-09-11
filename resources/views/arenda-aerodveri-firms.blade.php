c2a975f06301c7e8e557e569b2edbc49a8bd1e42
Mikhail Abramishvili <mg@abramishvili.net>
2023-09-11 18:40:21 +0400

    .

---
+++ resources/views/arenda-aerodveri-firms.blade.php
@@ -0,0 +1,44 @@
+@section('title', 'Аренда АэроДвери')
+@section('meta_description', 'Аренда АэроДвери')
+
+@extends('layouts.front')
+@section('content')
+    <div class="page policy">
+        <div class="container">
+            <h1 class="title-head">Аренда АэроДвери г. {{ $city->name }}</h1>
+            
+            @foreach($city->firms as $firm)
+                <div class="firm-card">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-lg-3">
+                            @foreach($firm->gallery as $galleryItem)
+                                @if($loop->first)
+                                    <div class="firm-pic" style="background-image: url({{ $galleryItem }});"></div>
+                                @endif
+                            @endforeach
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <div class="firm-card-desc">
+                                <h3>
+                                    @if(app()->getLocale() == 'en')
+                                        {{ $firm->name_eng }}
+                                    @else
+                                        {{ $firm->name }}
+                                    @endif
+                                </h3>
+    
+                                <span class="firm-price">от {{ $firm->price }} руб.</span>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-3">
+                            <div class="firm-card-button">
+                                <a href="/firm/{{ $firm->id }}" class="btn btn-standard">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+@endsection
--- resources/views/arenda-aerodveri.blade.php
+++ resources/views/arenda-aerodveri.blade.php
@@ -6,8 +6,36 @@
     <div class="page policy">
         <div class="container">
             <h1 class="title-head">Аренда АэроДвери</h1>
+
+            @if($settings->energoarenda_gallery)
+                <div class="energoaudit-gallery" data-flickity='{ "cellAlign": "left", "contain": true, "pageDots": false }'>
+                    @foreach($settings->energoarenda_gallery as $gal)
+                        <div class="carousel-cell">
+                            <img src="{{ $gal }}" alt="Аренда АэроДвери">
+                        </div>
+                    @endforeach
+                </div>
+            @endif
             
-            <p>Страница в разработке.</p>
+            <p class="cities-list-header">Выберите город:</p>
+
+            <ul class="cities-list">
+                @foreach($cities as $city)
+                    <li>
+                        <a href="/arenda-aerodveri/{{ $city->slug }}/firms">
+                            @if(app()->getLocale() == 'en')
+                                {{ $city->name_eng }}
+                            @else
+                                {{ $city->name }}
+                            @endif
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+
+            @if($settings->energoarenda_text)
+                {!! $settings->energoarenda_text !!}
+            @endif
         </div>
     </div>
 @endsection
