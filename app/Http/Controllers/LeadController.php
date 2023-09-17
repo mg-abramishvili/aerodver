52eac7565bb2130a9a602e11c4d720df4b8726c2
Mikhail Abramishvili <mg@abramishvili.net>
2023-09-17 20:51:37 +0400

    .

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -39,6 +39,6 @@ class LeadController extends Controller
             session()->forget('cart');
         }
 
-        Mail::to('aerodver.ru@mail.ru')->send(new LeadMail($lead));
+        Mail::to('mg@abramishvili.net')->send(new LeadMail($lead));
     }
 }
